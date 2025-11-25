96d0498832a4c88c21798db9b18c7f559abe195f
yagnesh parekh <yagneshparekh@rocketmail.com>
2025-11-25 12:05:42 -0500

    passing formatFunction as second argumnet for hook

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,11 +3,11 @@
 
 import * as React from 'react'
 
+const formatFunc = ({query, state}) => `\`${query}\` => ${state}`
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
-  // 🐨 call React.useDebugValue here.
-  // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
-  React.useDebugValue(`\`${query}\` => ${state}`)
+
+  React.useDebugValue({query, state}, formatFunc)
 
   React.useEffect(() => {
     let mounted = true
